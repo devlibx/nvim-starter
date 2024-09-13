340fb6263433deec306a0c12ef5c6a4e29cce651
Harish Bohara <harish.bohara@gmail.com>
2024-09-13 16:23:59 +0530

    [*] intermediate code change

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,12 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 
--- Formate file
--- <leader> s + f
-vim.api.nvim_set_keymap('n', '<leader>sf', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
-
-
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- -- Nvim DAP
 map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
@@ -22,10 +16,10 @@ map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugge
 map("n", "<Leader>dc>", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
 map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
 map(
-	"n",
-	"<Leader>dd",
-	"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
-	{ desc = "Debugger set conditional breakpoint" }
+  "n",
+  "<Leader>dd",
+  "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+  { desc = "Debugger set conditional breakpoint" }
 )
 map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
 map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
--- lua/plugins/setup.lua
+++ lua/plugins/setup.lua
@@ -1,5 +1,25 @@
-
 -- Add setup scripts here
+
+-- Define a command to format code using LSP
+vim.api.nvim_create_user_command('Fmt', function()
+  vim.lsp.buf.format()
+end, { desc = 'Format code using LSP' })
+
+-- Define a command to add, commit, and push changes to Git
+vim.api.nvim_create_user_command('GitPush', function()
+  -- Add all changes
+  vim.fn.system('git add .')
+
+  -- Commit changes with a default message
+  local commit_message = "[*] intermediate code change"
+  vim.fn.system('git commit -m "' .. commit_message .. '"')
+
+  -- Push changes
+  vim.fn.system('git push')
+
+  -- Notify user
+  print("Git operations completed: add, commit, push")
+end, { desc = 'Add, commit, and push changes to Git' })
 
 -- No-op
 return {}
